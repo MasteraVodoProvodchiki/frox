2da84e7f2d0c5f7d0d2df26195810a379f2f1111
Dmitry <gamexgroup@gmail.com>
2019-06-22 18:11:12 +0300

    upgrade utils

--- Frox/Utils.h
+++ Frox/Utils.h
@@ -4,6 +4,95 @@
 namespace frox {
 namespace utils {
 
+/* I0, O0 */
+template<typename LeftT, typename RightT,typename CallbackT>
+void Foreach(ComputeFramePtr left, ComputeFramePtr right, CallbackT callback)
+{
+	Size leftSize = left->GetSize();
+	Size rightSize = right->GetSize();
+
+	assert(leftSize == rightSize);
+
+	for (uint32_t row = 0; row < leftSize.Height; ++row)
+	{
+		const LeftT* leftValues = left->GetRowData<LeftT>(row);
+		RightT* rightValues = right->GetRowData<RightT>(row);
+
+		for (uint32_t column = 0; column < leftSize.Width; ++column)
+		{
+			callback(leftValues[column], rightValues[column]);
+		}
+	}
+}
+
+/* I0, O0 */
+template <typename LeftT, typename OperatorT>
+void Foreach(ComputeFramePtr left, ComputeFramePtr right, OperatorT callback)
+{
+	EComputeFrameType rightType = left->GetType();
+	switch (rightType)
+	{
+	case ECFT_Bool: {
+		Foreach<LeftT, bool>(left, right, callback);
+		break;
+	}
+	case ECFT_UInt8: {
+		Foreach<LeftT, uint8_t>(left, right, callback);
+		break;
+	}
+	case ECFT_UInt16: {
+		Foreach<LeftT, uint16_t>(left, right, callback);
+		break;
+	}
+	case ECFT_UInt32: {
+		Foreach<LeftT, uint32_t>(left, right, callback);
+		break;
+	}
+	case ECFT_Float: {
+		Foreach<LeftT, float>(left, right, callback);
+		break;
+	}
+	default:
+		assert(false);
+		break;
+	}
+}
+
+/* I0, O0 */
+template <typename OperatorT>
+void Foreach(ComputeFramePtr left, ComputeFramePtr right, OperatorT callback)
+{
+	EComputeFrameType leftType = left->GetType();
+
+	switch (leftType)
+	{
+	case ECFT_Bool: {
+		Foreach<bool>(left, right, callback);
+		break;
+	}
+	case ECFT_UInt8: {
+		Foreach<uint8_t>(left, right, callback);
+		break;
+	}
+	case ECFT_UInt16: {
+		Foreach<uint16_t>(left, right, callback);
+		break;
+	}
+	case ECFT_UInt32: {
+		Foreach<uint32_t>(left, right, callback);
+		break;
+	}
+	case ECFT_Float: {
+		Foreach<float>(left, right, callback);
+		break;
+	}
+	default:
+		assert(false);
+		break;
+	}
+}
+
+/* I0, I1, O0 */
 template<typename TypeT, typename CallbackT>
 void Foreach(ComputeFramePtr left, ComputeFramePtr right, ComputeFramePtr output, CallbackT callback)
 {
@@ -28,8 +117,9 @@ void Foreach(ComputeFramePtr left, ComputeFramePtr right, ComputeFramePtr output
 	}
 }
 
+/* I0, I1, O0 */
 template <typename OperatorT>
-void Foreach(ComputeFramePtr left, ComputeFramePtr right, ComputeFramePtr output)
+void Foreach(ComputeFramePtr left, ComputeFramePtr right, ComputeFramePtr output, OperatorT callback)
 {
 	EComputeFrameType leftType = left->GetType();
 	EComputeFrameType rightType = right->GetType();
@@ -41,20 +131,24 @@ void Foreach(ComputeFramePtr left, ComputeFramePtr right, ComputeFramePtr output
 
 	switch (type)
 	{
+	case ECFT_Bool: {
+		Foreach<bool>(left, right, output, callback);
+		break;
+	}
 	case ECFT_UInt8: {
-		Foreach<uint8_t>(left, right, output, OperatorT());
+		Foreach<uint8_t>(left, right, output, callback);
 		break;
 	}
 	case ECFT_UInt16: {
-		Foreach<uint16_t>(left, right, output, OperatorT());
+		Foreach<uint16_t>(left, right, output, callback);
 		break;
 	}
 	case ECFT_UInt32: {
-		Foreach<uint32_t>(left, right, output, OperatorT());
+		Foreach<uint32_t>(left, right, output, callback);
 		break;
 	}
 	case ECFT_Float: {
-		Foreach<float>(left, right, output, OperatorT());
+		Foreach<float>(left, right, output, callback);
 		break;
 	}
 	default:
@@ -74,6 +168,20 @@ void Foreach(ComputeFramePtr frame, CallbackT callback)
 		for (uint32_t column = 0; column < size.Width; ++column)
 		{
 			callback(values[column]);
+		}
+	}
+}
+
+template <typename TypeT, typename CallbackT>
+void Fill(ComputeFramePtr frame, CallbackT callback)
+{
+	Size size = frame->GetSize();
+	for (uint32_t row = 0; row < size.Height; ++row)
+	{
+		TypeT* values = frame->GetRowData<TypeT>(row);
+		for (uint32_t column = 0; column < size.Width; ++column)
+		{
+			values[column] = callback(values[column]);
 		}
 	}
 }
