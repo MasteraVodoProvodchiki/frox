cd98d8b683ff7b45313aa23e82d19bea4a6aeece
Dmitry <gamexgroup@gmail.com>
2019-07-14 15:37:49 +0300

    update depth sesnor tests

--- FroxTest/DepthSensorTest.cpp
+++ FroxTest/DepthSensorTest.cpp
@@ -1,18 +1,29 @@
 #include "Tests.h"
 #include "Common.h"
 
+#include <Log.h>
+
 #include <DepthSensorModule.h>
 #include <SensorFrameNode.h>
+#include <MOCSensorDeviceManager.h>
+#include <RealSenseModule.h>
 
 using namespace frox;
 
 bool sensorFrameTest0(ComputeFlow& flow, FlowPerformer& performer, FlowData& inputData, FlowData& ouputData)
 {
+	// Add MOC
+	auto mocDeviceManager = IDepthSensorModule::Get().CreateDeviceManager<MOCSensorDeviceManager>();
+	auto mocDevice = mocDeviceManager->CreateDevice();
+	assert(mocDevice);
+
 	// Create nodes
 	auto sensorDepthFrame = flow.CreateNode<SensorDepthFrameNode>("SensorDepthFrame");
+	sensorDepthFrame->SetSensorSerial(mocDevice->GetSerial());
 	flow.ConnectOutput(flow.CreateOutput("out"), sensorDepthFrame);
 
-	return runFlow(
+	// Test
+	auto result = runFlow(
 		flow,
 		performer,
 		inputData,
@@ -22,6 +33,42 @@ bool sensorFrameTest0(ComputeFlow& flow, FlowPerformer& performer, FlowData& inp
 			return true;
 		}
 	);
+
+	// Remove MOC
+	IDepthSensorModule::Get().UnRegisterDeviceManager(mocDeviceManager.get());
+	return result;
+}
+
+bool sensorRealSenseTest0(ComputeFlow& flow, FlowPerformer& performer, FlowData& inputData, FlowData& ouputData)
+{
+	// Add RealSense
+	IRealSenseModule::Get().InitialiseModule();
+	if (IDepthSensorModule::Get().GetNumDevices() == 0)
+	{
+		Log::Warning("Depth sensors not found");
+		return false;
+	}
+
+	// Create nodes
+	auto sensorDepthFrame = flow.CreateNode<SensorDepthFrameNode>("SensorDepthFrame");
+	flow.ConnectOutput(flow.CreateOutput("out"), sensorDepthFrame);
+
+	// Test
+	auto result = runFlow(
+		flow,
+		performer,
+		inputData,
+		ouputData,
+		[](ComputeFramePtr result) {
+			// Check
+			return true;
+		}
+	);
+
+	// Remove RealSense
+	IRealSenseModule::Get().ShutdownModule();
+
+	return result;
 }
 
 void Tests::DepthSensorTest()
@@ -29,5 +76,6 @@ void Tests::DepthSensorTest()
 	IDepthSensorModule::Get();
 
 	using namespace std::placeholders;
-	test("SensorFrameTest", std::bind(&sensorFrameTest0, _1, _2, _3, _4));
+	test("SensorFrame", std::bind(&sensorFrameTest0, _1, _2, _3, _4));
+	test("RealSense", std::bind(&sensorRealSenseTest0, _1, _2, _3, _4));
 }
