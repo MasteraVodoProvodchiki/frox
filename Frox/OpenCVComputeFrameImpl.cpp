9c5ed437cf700a3c954d9020b1df5ae23c91bd9a
Dmitry <gamexgroup@gmail.com>
2019-07-21 13:11:50 +0300

    add opencv frame

---
+++ Frox/OpenCVComputeFrameImpl.cpp
@@ -0,0 +1,126 @@
+#include "OpenCVComputeFrameImpl.h"
+#include <assert.h>
+
+namespace frox {
+
+OpenCVComputeFrameImpl::OpenCVComputeFrameImpl(Size size, ComputeFrameType typeData, const void* data)
+	: _type(typeData)
+{	
+	assert(size.Width > 0 && size.Height > 0);
+	assert(typeData.Type != EComputeFrameType::ECFT_None && typeData.Channels > 0 && typeData.Channels <= 4);
+
+	void* unconstData = const_cast<void*>(data);
+
+	uint32_t channels = typeData.Channels;
+	switch (typeData.Type)
+	{
+	case ECFT_Bool:
+		_data = data != nullptr ? 
+			cv::Mat(cv::Size(size.Width, size.Height), CV_8UC(channels), unconstData).clone() :
+			cv::Mat(cv::Size(size.Width, size.Height), CV_8UC(channels));
+		break;
+	case ECFT_UInt8:
+		_data = data != nullptr ?
+			cv::Mat(cv::Size(size.Width, size.Height), CV_8UC(channels), unconstData).clone() :
+			cv::Mat(cv::Size(size.Width, size.Height), CV_8UC(channels));
+		break;
+	case ECFT_UInt16:
+		_data = data != nullptr ?
+			cv::Mat(cv::Size(size.Width, size.Height), CV_16UC(channels), unconstData).clone() :
+			cv::Mat(cv::Size(size.Width, size.Height), CV_16UC(channels));
+		break;
+	case ECFT_UInt32:
+		_data = data != nullptr ?
+			cv::Mat(cv::Size(size.Width, size.Height), CV_32SC(channels), unconstData).clone() :
+			cv::Mat(cv::Size(size.Width, size.Height), CV_32SC(channels));
+		break;
+	case ECFT_Float:
+		_data = data != nullptr ?
+			cv::Mat(cv::Size(size.Width, size.Height), CV_32FC(channels), unconstData).clone() :
+			cv::Mat(cv::Size(size.Width, size.Height), CV_32FC(channels));
+		break;
+	default:
+		assert(false);
+		break;
+	}
+}
+
+OpenCVComputeFrameImpl::~OpenCVComputeFrameImpl()
+{}
+
+ComputeFrameType OpenCVComputeFrameImpl::GetType() const
+{
+	return _type;
+}
+
+uint32_t OpenCVComputeFrameImpl::GetChannels() const
+{
+	return _type.Channels;
+}
+
+uint32_t OpenCVComputeFrameImpl::GetElementSize() const
+{
+	return utils::FrameTypeToSize(_type.Type) * _type.Channels;
+}
+
+bool OpenCVComputeFrameImpl::IsValid() const
+{
+	return _data.size() != cv::Size(0, 0);
+}
+
+Size OpenCVComputeFrameImpl::GetSize() const
+{
+	cv::Size size = _data.size();
+	return Size{
+		uint32_t(size.width),
+		uint32_t(size.height)
+	};
+}
+
+const void* OpenCVComputeFrameImpl::GetData() const
+{
+	return _data.data;
+}
+
+const void* OpenCVComputeFrameImpl::GetRowData(uint32_t row) const
+{
+	cv::Size size = _data.size();
+	assert(row < size.height);
+	assert(_data.isContinuous());
+
+	return reinterpret_cast<uint8_t*>(_data.data) + (row * size.height) * GetElementSize();
+}
+
+const void* OpenCVComputeFrameImpl::At(uint32_t row, uint32_t column) const
+{
+	cv::Size size = _data.size();
+	assert(row < size.height && column < size.width);
+	assert(_data.isContinuous());
+
+	return reinterpret_cast<uint8_t*>(_data.data) + (row * size.width + column) * GetElementSize();
+}
+
+void* OpenCVComputeFrameImpl::GetData()
+{
+	return _data.data;
+}
+
+void* OpenCVComputeFrameImpl::GetRowData(uint32_t row)
+{
+	cv::Size size = _data.size();
+	assert(row < size.height);
+	assert(_data.isContinuous());
+
+	return reinterpret_cast<uint8_t*>(_data.data) + (row * size.height) * GetElementSize();
+}
+
+void* OpenCVComputeFrameImpl::At(uint32_t row, uint32_t column)
+{
+	cv::Size size = _data.size();
+	assert(row < size.height && column < size.width);
+	assert(_data.isContinuous());
+
+	return reinterpret_cast<uint8_t*>(_data.data) + (row * size.width + column) * GetElementSize();
+}
+
+} // End frox
---
+++ Frox/OpenCVComputeFrameImpl.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "ComputeFrame.h"
+#include "OpenCV.h"
+
+namespace frox {
+
+/**
+ * OpenCV ComputeFrame with cv::Mat
+ */
+class OpenCVComputeFrameImpl : public ComputeFrame
+{
+public:
+	OpenCVComputeFrameImpl(Size size = Size{ 0, 0 }, ComputeFrameType type = ComputeFrameType{ ECFT_None, 0 }, const void* data = nullptr);
+	virtual ~OpenCVComputeFrameImpl() override;
+
+	static ComputeFramePtr Create(Size size = Size{ 0, 0 }, ComputeFrameType type = ComputeFrameType{ ECFT_None, 0 }, const void* data = nullptr)
+	{
+		return std::make_shared<OpenCVComputeFrameImpl>(size, type, data);
+	}
+
+	// ComputeFrame overrides
+	virtual ComputeFrameType GetType() const override;
+	virtual uint32_t GetChannels() const override;
+	virtual uint32_t GetElementSize() const override;
+	virtual bool IsValid() const override;
+	virtual Size GetSize() const override;
+	virtual const void* GetData() const override;
+	virtual const void* GetRowData(uint32_t row) const override;
+	virtual const void* At(uint32_t row, uint32_t column) const override;
+	virtual void* GetData() override;
+	virtual void* GetRowData(uint32_t row) override;
+	virtual void* At(uint32_t row, uint32_t column) override;
+
+private:
+	ComputeFrameType _type;
+	cv::Mat _data;
+};
+
+} // End frox
